a06d858308fad0ebf82eeec4d95cf2d57c013833
Wen bin <905613455@qq.com>
2018-08-03 04:21:32 +0000

    Add password resets & email configuration

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -97,13 +97,12 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = '18612636812@163.com';
-        $name = 'Wenbin';
+
         $to = $user->email;
         $subject = "感谢注册 Sample 应用！ 请确认你的邮件。";
 
-        Mail::send($view,$data,function ($message) use ($from ,$name, $to ,$subject){
-            $message->from($from , $name)->to($to)->subject($subject);
+        Mail::send($view,$data,function ($message) use ($to ,$subject){
+            $message->to($to)->subject($subject);
         });
     }
 
